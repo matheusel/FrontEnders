41e2431986254e6ab49c6de0c62dfba6b6d604b1
Matheus El <matheuselh@gmail.com>
2022-11-07 21:44:14 -0300

    adição de erro quando enviar o submit

--- Login-Cadastro/Cadastro/script.js
+++ Login-Cadastro/Cadastro/script.js
@@ -1,15 +1,16 @@
-let inputUser = document.getElementById("user").value;
-let inputPassword = document.getElementById("password").value;
-let inputPasswordConfirm = document.getElementById("passwordconfirm").value;
-let inputRG = document.getElementById("rg").value;
-let inputCEP = document.getElementById("cep").value;
-let inputEstado = document.getElementById("estado").value;
-let inputCidade = document.getElementById("cidade").value;
-let inputBairro = document.getElementById("bairro").value;
-let inputRua = document.getElementById("rua").value;
-let inputNumero = document.getElementById("numero").value;
-let inputComplemento = document.getElementById("complemento").value;
-let form = document.getElementById("form");
+const InputEmail = document.getElementById("user").value;
+const InputEmailCaixa = document.getElementById("user");
+const inputPassword = document.getElementById("password");
+const inputPasswordConfirm = document.getElementById("passwordconfirm");
+const inputRG = document.getElementById("rg");
+const inputCEP = document.getElementById("cep").value;
+const inputEstado = document.getElementById("estado");
+const inputCidade = document.getElementById("cidade");
+const inputBairro = document.getElementById("bairro");
+const inputRua = document.getElementById("rua");
+const inputNumero = document.getElementById("numero");
+const inputComplemento = document.getElementById("complemento");
+const form = document.getElementById("form");
 
 //API CEP
 
@@ -48,8 +49,9 @@ const pesquisarCEP = async () => {
         if (endereco.hasOwnProperty("erro")){
             document.getElementById("cepError").innerText = "Seu CEP não foi encontrado"
             document.getElementById("cepError").className += "d-flex";
-    
+            document.getElementById("cep").className += " is-invalid"    
         } else {
+            document.getElementById("cep").classList.remove("is-invalid")
             document.getElementById("cepError").innerText = ""
             document.getElementById("cepError").className += "d-none";
             preencherInput(endereco)
@@ -59,19 +61,81 @@ const pesquisarCEP = async () => {
         document.getElementById("cepError").innerText = ""
         document.getElementById("cepError").innerText = "Seu CEP é inválido"
         document.getElementById("cepError").className += "d-flex";
+        document.getElementById("cep").className += " is-invalid"   
     }
 }
 
-//CHAMA A FUNÇÃO QUANDO DESFOCA DO INPUT
+//CONFERIR SE OS INPUTS ESTÃO PREENCHIDOS CORRETAMENTE
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email);
+  }
+
+function setError(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small")
+
+    small.innerText = message;
+    small.classList.remove("d-none")
+
+    input.className += " is-invalid"  
+}
+
+function conferirInputs(){
+    //conferir se foi preenchido os campos email
+    if (InputEmailCaixa.value === ""){
+        setError(InputEmailCaixa, "Preencha este campo")
+    } else if (!checkEmail(InputEmailCaixa.value)) {
+        setError(InputEmailCaixa, "Este email é inválido")
+    }
+    //conferir se foi preenchido os campos senha
+    if (inputPassword.value === ""){
+        setError(inputPassword, "Preencha este campo");
+    }
+    
+    if (inputPassword.value !== inputPasswordConfirm.value){
+        setError(inputPasswordConfirm, "As senhas não conferem");
+    }
+
+    //conferir se foi preenchido os campos rg
+
+    if (inputRG.value === ""){
+        setError(inputRG, "Preencha este campo");
+    }
+
+    if (inputEstado.value === ""){
+        setError(inputEstado, "Preencha este campo");
+    }
+
+    if (inputCidade.value === ""){
+        setError(inputCidade, "Preencha este campo");
+    }
+
+    if (inputBairro.value === ""){
+        setError(inputBairro, "Preencha este campo");
+    }
+
+    if (inputRua.value === ""){
+        setError(inputRua, "Preencha este campo");
+    }
+
+    if (inputNumero.value === ""){
+        setError(inputNumero, "Preencha este campo");
+    }
+
+    if (inputComplemento.value === ""){
+        setError(inputComplemento, "Preencha este campo");
+    }
+
+}
+
+//CHAMA A FUNÇÃO pesquisarCEP QUANDO DESFOCA DO INPUT
 document.getElementById("cep").addEventListener("focusout", pesquisarCEP)
-
-
-//SENHA E EMAIL
 
 //BOTÃO ENVIAR
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault()
-
-    
+    conferirInputs()
 })
